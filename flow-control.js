41ac5cbda1a3c3051a57a02523bfa6aa98aa8d89
Jesse Markowitz <jess.markowitz@gmail.com>
2020-03-07 02:11:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,28 +28,9 @@ return age>=13&&age<=19 ? "You are a teenager" : "You are not a teenager"
 
 function switchAge(age) {
   switch(age) {
-    case age===13:
-      console.log("You are a teenager")
-      break;
-    case age===14:
-      console.log("You are a teenager")
-      break;
-    case age===15:
-      console.log("You are a teenager")
-      break;
-    case age===16:
-      console.log("You are a teenager")
-      break;
-    case age===17:
-      console.log("You are a teenager")
-      break;
-    case age===18:
-      console.log("You are a teenager")
-      break;
-    case age===19:
-      console.log("You are a teenager")
-      break;
+    case age>=13 && age<=19:
+      return "You are a teenager"
     default:
-      console.log("You have an age") 
+      return "You have an age"
   }
 }
